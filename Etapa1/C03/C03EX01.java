1a1b88e9b491488477755d89be6406931e2f361f
oEnzoRibas <enzorochaleitedinizribas@gmail.com>
2024-10-03 20:27:33 -0300

    teste 1

--- Etapa1/C03/C03EX01.java
+++ Etapa1/C03/C03EX01.java
@@ -2,7 +2,7 @@ package Etapa1.C03;
 
 import java.util.Scanner;
 public class C03EX01 {
-    // Função : Calcular o fatorial de um número
+    // Função : Calcular o fatorial de um número qualquer
     // Autor : Enzo Rocha Leite Diniz Ribas
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
